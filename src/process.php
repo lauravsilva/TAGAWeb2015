3bf447bb4342d3ccf75229e55583dad53f5d3681
mandileighh <alm3831@rit.edu>
2015-12-08 17:12:37 -0500

    added process page with php includes

---
+++ src/process.php
@@ -0,0 +1,34 @@
+<?php
+	$title = "TAGA | The Process";
+	$active_page = "process";
+	include "header.php";
+?> 
+
+<header></header>
+  
+    <article class="process">
+    <h1>The Process</h1>
+      <h2>Fundraising</h2>
+      <p>Raise money for TAGA through various events, including bake sales, t-shirt sales, and other public events. Collaborate with the Budget Team <!--Is there a Budget Team?--> to ascertain TAGA funds.</p>
+      <img alt="fundraising icon" src="#">
+      <h2>Copy Editing</h2>
+      <p>Curate suitable papers about printing and publishing. Perform editing to ensure they are professional and grammatically correct.</p>
+      <img alt="editing icon" src="#">
+      <h2>Creative &amp; Design</h2>
+      <p>Manage the look and functionality of our book and ensure visual consistency across platforms. Ideate innovative solutions to make our book unique.</p>
+      <img alt="design icon" src="#">
+      <h2>Photo &amp; Video</h2>
+      <p>Create photos and videos of our process to be used for our website, app, and fundraising content.</p>
+      <img alt="photo icon" src="#">
+      <h2>Website</h2>
+      <p>Design our website to be user-friendly and carry our current theme into an online platform. Ensure proper mobile and tablet optimization.</p>
+      <img alt="website icon" src="#">
+      <h2>Digital</h2>
+      <p>Create a digital publication that complements the printed edition using Adobe Digital Publishing Suite. Create a digital book for iPad and iPhone, utilizing proper UI/UX design.</p>
+      <img alt="digital icon" src="#">
+      <h2>Print</h2>
+      <p>Print a high quality book using various printing methods such as flexography, digital printing, lithography, and 3D printing. Ensure quality and cost effectiveness throughout the entire process.</p>
+      <img alt="print icon" src="#">
+      </article>
+
+<?php include('footer.php'); ?>
