6db610099bec02b8b88010a0e7012de158d6b542
mandileighh <alm3831@rit.edu>
2015-12-08 17:09:31 -0500

    added members page with php includes

---
+++ src/members.php
@@ -0,0 +1,152 @@
+<?php
+	$title = "TAGA | Members";
+	$active_page = "members";
+	include "header.php";
+?> 
+
+<div class="container membersPage">
+            
+            <ul class="teams"> 
+                <li>Creative / Design</li>
+                <li>Digital</li>
+                <li>Fundraising</li>
+                <li>Photo / Video</li>
+                <li>Print</li>
+                <li>Web</li>
+            </ul>
+            
+            <h1>Eboard</h1>
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="squareSolidEboard">
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="squareEboard">
+                        <img src="#" alt="" />
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="squareEboard">
+                        <img src="#" alt="" />
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="squareSolidEboard">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="squareEboard">
+                        <img src="#" alt="" />
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="squareEboard">
+                        <img src="#" alt="" />
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="squareEboard">
+                        <img src="#" alt="" />
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="squareEboard">
+                        <img src="#" alt="" />
+                    </div>
+                </div>
+            </div>
+
+            <h1>Members</h1>
+            <div class="row members">
+                <div class="col-md-2">
+                    <div class="squareMember">
+                        <img src="#" alt="" />
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="squareSolid">
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="squareMember">
+                        <img src="#" alt="" />
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="squareSolid">
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="squareMember">
+                        <img src="#" alt="" />
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="squareSolid">
+                    </div>
+                </div>
+            </div>
+            <div class="row members">
+                <div class="col-md-2">
+                    <div class="squareSolid">
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="squareMember">
+                        <img src="#" alt="" />
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="squareSolid">
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="squareMember">
+                        <img src="#" alt="" />
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="squareSolid">
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="squareMember">
+                        <img src="#" alt="" />
+                    </div>
+                </div>
+            </div>
+            <div class="row members">
+                <div class="col-md-2">
+                    <div class="squareMember">
+                        <img src="#" alt="" />
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="squareSolid">
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="squareMember">
+                        <img src="#" alt="" />
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="squareSolid">
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="squareMember">
+                        <img src="#" alt="" />
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="squareSolid">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+<?php include('footer.php'); ?>
