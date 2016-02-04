11b0bcdf7325ec83fcd0ffd7090893018ffa13d9
meghanolexa <mxo5334@rit.edu>
2016-02-03 23:07:13 -0500

    Added correct icons and edited pages

--- src/process.php
+++ src/process.php
@@ -4,31 +4,61 @@
 	include "header.php";
 ?> 
 
-<header></header>
-  
-    <article class="process">
+    <div class="process">
     <h1>The Process</h1>
+        <div class="content">
+        
+            <img alt="fundraising icon" src="assets/process-fundraising.png" class="left">
+        
+            <div class="fundraising blue">
       <h2>Fundraising</h2>
-      <p>Raise money for TAGA through various events, including bake sales, t-shirt sales, and other public events. Collaborate with the Budget Team <!--Is there a Budget Team?--> to ascertain TAGA funds.</p>
-      <img alt="fundraising icon" src="#">
+      <p>Raise money for TAGA through various events, including bake sales, t-shirt sales, and other public events.</p>
+        </div>
+        
+            <img alt="editing icon" src="assets/process-copyediting.png" class="right">
+        
+            <div class="copy-editing pink">
       <h2>Copy Editing</h2>
-      <p>Curate suitable papers about printing and publishing. Perform editing to ensure they are professional and grammatically correct.</p>
-      <img alt="editing icon" src="#">
+      <p>Curate suitable papers about printing and publishing. Edit papers to ensure they are professional and grammatically correct.</p>
+        </div>
+        
+            <img alt="design icon" src="assets/process-design.png" class="left">
+        
+            <div class="design blue">
       <h2>Creative &amp; Design</h2>
       <p>Manage the look and functionality of our book and ensure visual consistency across platforms. Ideate innovative solutions to make our book unique.</p>
-      <img alt="design icon" src="#">
+        </div>
+        
+            <img alt="photo icon" src="assets/process-photo.png" class="right">
+        
+            <div class="photo pink">
       <h2>Photo &amp; Video</h2>
       <p>Create photos and videos of our process to be used for our website, app, and fundraising content.</p>
-      <img alt="photo icon" src="#">
+        </div>
+        
+            <img alt="website icon" src="assets/process-web.png" class="left">
+        
+            <div class="web blue">
       <h2>Website</h2>
       <p>Design our website to be user-friendly and carry our current theme into an online platform. Ensure proper mobile and tablet optimization.</p>
-      <img alt="website icon" src="#">
+        </div>
+        
+            <img alt="digital icon" src="assets/process-digital.png" class="right">
+        
+            <div class="digital pink">
       <h2>Digital</h2>
       <p>Create a digital publication that complements the printed edition using Adobe Digital Publishing Suite. Create a digital book for iPad and iPhone, utilizing proper UI/UX design.</p>
-      <img alt="digital icon" src="#">
+        </div>
+        
+            <img alt="print icon" src="assets/process-print.png" class="left">
+        
+        <div class="print blue">
       <h2>Print</h2>
       <p>Print a high quality book using various printing methods such as flexography, digital printing, lithography, and 3D printing. Ensure quality and cost effectiveness throughout the entire process.</p>
-      <img alt="print icon" src="#">
-      </article>
+        </div>
+        
+        </div>
+      
+      </div>
 
 <?php include('footer.php'); ?>
