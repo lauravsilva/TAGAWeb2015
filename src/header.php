564d5a776ed83be5e99d8b395edae01ea1233c49
mandileighh <alm3831@rit.edu>
2016-03-18 10:55:58 -0400

    two extra end tags. fixed it.

--- src/header.php
+++ src/header.php
@@ -35,7 +35,6 @@
                         <li <?= $active_page == "home" ? "class='active'": ""?>><div class="navlink"><a href="index.php">HOME</a></div></li>
                         <li <?=$active_page == "about" ? "class='active'": ""?>><div class="navlink"><a href="members.php">MEMBERS</a></div></li>
                         <li <?=$active_page == "process" ? "class='active'": ""?>><div class="navlink"><a href="process.php">PROCESS</a></div></li>
-                            </li>
                         <li><div class="navbar-brand"><a href="index.php"><img src="assets/logo_desktop.png" id="tagalogo"></a></div></li>
                         <li <?= $active_page == "portfolio" ? "class='active'": ""?>><div class="navlink"><a href="portfolio.php">PORTFOLIO</a></div></li>
                         <li <?= $active_page == "blog" ? "class='active'": ""?>><div class="navlink"><a href="https://rittaga.wordpress.com/" target="_blank">BLOG</a></div></li>
